7157cfc8b28268f44b1bcc41777b4c71fdb5026f
Byeongjo Kim <bjbj8083@gmail.com>
2019-08-07 14:30:43 +0900

    train fin

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import argparse
+
 from model import Generator, Discriminator
+from data import Celeba_Dataset
+
+import numpy as np
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from data import Celeba_Dataset
 from torch.utils.data import DataLoader
+
 import torchvision.transforms as transforms
+import torchvision.utils as vutils
 
 def train(args):
     def weights_init(m):
@@ -44,14 +50,14 @@ def train(args):
                               shuffle=True,
                               num_workers=args.workers)
 
+    fixed_noise = torch.randn(args.save_img, args.length_z, 1, 1, device=device)
+
     for epoch in range(args.epoch):
         D_losses = []
         G_losses = []
         for i, data in enumerate(train_loader):
-
             netD.zero_grad()
             real_img = data.to(device)
-            print(real_img.shape)
             b_size = real_img.size(0)
             real_label = torch.ones((b_size), device=device)
             real_output = netD(real_img).view(-1)
@@ -59,7 +65,6 @@ def train(args):
             errD_real.backward()
 
             noise = torch.randn(b_size, args.length_z, 1, 1, device=device)
-            print(noise.shape)
             fake_img = netG(noise)
             fake_label = torch.zeros((b_size), device=device)
             fake_output = netD(fake_img.detach()).view(-1)
@@ -78,11 +83,27 @@ def train(args):
             D_losses.append(errD.item())
             G_losses.append(errG.item())
 
-            
-            break
-        print()
-        break
+            if(i%100 == 0):
+                print(errD.item(), errG.item())
+                with torch.no_grad():
+                    fake_img = netG(fixed_noise)
+                    for img_no in range(args.save_img):
+                        vutils.save_image(fake_img[img_no], "./output/" + str(epoch) + "_" + str(i) + "_" + str(img_no) + ".jpg")
 
+        print(str(epoch) + " epoch\n")
+        print("\tD loss: " + str(np.mean(np.array(D_losses))))
+        print("\tG loss: " + str(np.mean(np.array(G_losses))))
+
+        with torch.no_grad():
+            fake_img = netG(fixed_noise)
+            for img_no in range(args.save_img):
+                vutils.save_image(fake_img[img_no], "./output/"+str(epoch)+"_"+str(img_no)+".jpg")
+
+        if(epoch % args.save_checkpoint_interval == 0):
+            torch.save({
+                "Generator_state_dict": netG.state_dict(),
+                "Discriminator_state_dict": netD.state_dict(),
+            }, args.checkpoint_path + str(epoch) + "_checkpoint.pth.tar")
 
 def test(args):
     return 1
@@ -100,18 +121,20 @@ if __name__ == '__main__':
     parser.add_argument('--image_size', type=int, nargs='+', default=64)
 
     # ckpt setting
-    parser.add_argument('--save_checkpoint_interval', type=int, default=20)
+    parser.add_argument('--save_checkpoint_interval', type=int, default=2)
+    parser.add_argument('--checkpoint_path', type=str, default="./_model/")
 
     # network setting
     parser.add_argument('--length_z', type=int, default=100)
 
     # training setting
-    parser.add_argument('--epoch', type=int, default=100)
+    parser.add_argument('--epoch', type=int, default=10)
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--lrG', type=float, default=0.0002)
     parser.add_argument('--lrD', type=float, default=0.0002)
     parser.add_argument('--beta1', type=float, default=0.5)
     parser.add_argument('--workers', type=int, default=2)
+    parser.add_argument('--save_img', type=int, default=2)
 
     # gpu setting
     parser.add_argument('--ngpu', type=int, default=1)
